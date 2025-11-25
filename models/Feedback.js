da4fd62aaefe529e6c37a4e0400db5de32411583
leclerc92 <leclerc92@gmail.com>
2025-11-25 19:10:18 +0100

    ajout du dossier models

---
+++ models/Feedback.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+    recipeId: {
+        type: Number,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
